770f087e72028336776a8579d5f29b3d4020e7b7
alfred-kctang <kctang@link.cuhk.edu.hk>
2021-03-24 00:02:37 +0800

    Second commit

--- starter/app.py
+++ starter/app.py
@@ -1,28 +1,93 @@
 import os
+import json
 
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask_migrate import Migrate
 
-#from models import
+from models import create_app, setup_db, setup_movie_model, setup_actor_model
 
-def create_app(test_config=None):
-  # Create and configure the app
-  app = Flask(__name__)
+# app config
+app = create_app()
+db = setup_db(app)
+migrate = Migrate(app, db)
 
-  # Set up CORS that allows any origins for the api resources
-  cors = CORS(app, resources={r"/api/*": {"origin": "*"}})
+# models
+Movie = setup_movie_model(db)
+Actor = setup_actor_model(db)
 
-  # Set Access-Control-Allow headers and methods
-  @app.after_request
-  def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
-    return response
+# pagination
+items_per_page = 100
+def paginate_results(request, items):
+  page = request.args.get('page', 1, type=int)
+  start = (page - 1) * items_per_page
+  end = start + items_per_page
+  current_items = items[start:end]
+  return current_items
 
-  return app
+# controllers
+@app.route('/movies', methods=['GET'])
+def get_movies():
+  page = request.args.get('page', 1, type=int)
+  movies = Movie.query.all()
+  if len(movies) == 0:
+    abort(404)
+  else:
+    paginated_movies = Movie.query.order_by(
+      Movie.year).paginate(page=page, per_page=items_per_page).items
+    
+  return jsonify({
+    'success': True,
+    'movies': {movie.id: {
+      'title': movie.title,
+      'year': movie.year,
+      'director': movie.director} for movie in paginated_movies}
+  })
 
-APP = create_app()
+@app.route('/actors', methods=['GET'])
+def get_actors():
+  page = request.args.get('page', 1, type=int)
+  actors = Actor.query.all()
+  if len(actors) == 0:
+    abort(404)
+  else:
+    paginated_actors = Actor.query.order_by(Actor.id).paginate(
+      page=page, per_page=items_per_page).items
+  
+  return jsonify({
+    'success': True,
+    'actors': {actor.id: {
+      'name': actor.name,
+      'film_title': actor.movie.title,
+      'character_type': actor.character_type
+    } for actor in paginated_actors}
+  })
+
+@app.route('/movies/<movie_id>', methods=['DELETE'])
+def delete_movies(movie_id):
+  try:
+    movie = Movie.query.get(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Movie ID ' + str(movie_id) + ' was successfully deleted!')
+  except:
+    db.session.rollback()
+    flash('An error occurred. Movie ID ' + str(movie_id) + ' could not be deleted.')
+  finally:
+    db.session.close()
+
+@app.route('/actors/<actor_id>', methods=['DELETE'])
+def delete_actors(actor_id):
+  try:
+    actor = Actor.query.get(actor_id)
+    db.session.delete(actor)
+    db.session.commit()
+    flash('Actor ID ' + str(actor_id) + ' was successfully deleted!')
+  except:
+    db.session.rollback()
+    flash('An error occurred. Actor ID ' + str(actor_id) + ' could not be deleted.')
+  finally:
+    db.session.close()
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
--- starter/models.py
+++ starter/models.py
@@ -1,21 +1,61 @@
 import os
 import json
 
-from sqlalchemy import
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
+from flask_cors import CORS
 
-database_name = ""
-database_path = "postgres://{}{}".format('localhost:5432', database_name)
+database_name = "film"
+database_path = "postgres://{}/{}".format('postgres@localhost:5432', database_name)
 
 db = SQLAlchemy()
 
+# Initialize flask app.
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__)
+    # Set up CORS that allows any origins for the api resources
+    cors = CORS(app, resources={r"/api/*": {"origin": "*"}})
+    moment = Moment(app)
+
+    # Set Access-Control-Allow headers and methods
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+    
+    return app
+
+# Conncet app and database.
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
+    db = SQLAlchemy(app)
+    return db
+    #db.init_app(app)
+    #db.create_all()
 
-class Movie(db.Model):
+# Create model for movie.
+def setup_movie_model(db):
+    class Movie(db.Model):
+        __tablename__ = 'Movie'
+        
+        id = db.Column(db.String, primary_key=True, nullable=False)
+        title = db.Column(db.String(500))
+        year = db.Column(db.String(120))
+        director = db.Column(db.String(120))
+        actors = db.relationship("Actor", backref="movie")
+    return Movie
 
-class Actor(db.Model):
+# Create model for actor.
+def setup_actor_model(db):
+    class Actor(db.Model):
+        __tablename__ = 'Actor'
+        
+        id = db.Column(db.Integer, primary_key=True, nullable=False)
+        film_id = db.Column(db.String, db.ForeignKey('Movie.id'), nullable=False)
+        name = db.Column(db.String(120))
+        character_type = db.Column(db.String(120))
+    return Actor 
